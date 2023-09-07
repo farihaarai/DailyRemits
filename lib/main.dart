52336d43fd389a84f15d65b43a7e6d59bd8474ae
farihashahid111 <farihashahid111@gmail.com>
2023-09-07 16:02:29 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
 var kDarkColorScheme = ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(255, 9, 99, 125),
-    brightness: Brightness.dark);
+  seedColor: const Color.fromARGB(255, 9, 99, 125),
+  brightness: Brightness.dark,
+);
 
 void main() {
   runApp(MaterialApp(
